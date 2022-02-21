94276634cc91826a4b521f4d6dfc82c286c1c9bd
harry <tlqfk__@naver.com>
2022-02-21 17:11:19 +0900

    22-02-21

---
+++ SPRING-BOOT_CRUD_WITH_SPRING-MVC_SPRING-DATA-JPA_THYMELEAF_HIBERNATE_MYSQL/src/main/java/com/example/springboot_crud_with_springmvc_springdatajpa_thymeleaf_hibernate_mysql/application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_crud_with_springmvc_springdatajpa_thymeleaf_hibernate_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(application.class, args);
+    }
+
+}
---
+++ SPRING-BOOT_CRUD_WITH_SPRING-MVC_SPRING-DATA-JPA_THYMELEAF_HIBERNATE_MYSQL/src/test/java/com/example/springboot_crud_with_springmvc_springdatajpa_thymeleaf_hibernate_mysql/applicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_crud_with_springmvc_springdatajpa_thymeleaf_hibernate_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class applicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
